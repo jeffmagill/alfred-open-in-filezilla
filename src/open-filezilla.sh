9f9da1720f70fc01a986dc7f9bca5820e8595c5b
Jeff <jeff@unhingedweb.com>
2013-10-02 21:38:42 -0400

    initial commit

---
+++ src/open-filezilla.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "<?xml version=\"1.0\"?>"
+echo "<items>"
+
+grep -io "<name>.*</name>" ~/.filezilla/sitemanager.xml | grep -io ">.*<" | grep -io "[^<]*" | cut -c2-10000 | grep -io "^\w.*$" | while read line; do
+	set -- $line
+	if [[ $line =~ {query}.* ]] ; then
+		echo "<item uid='$line' arg='$line' valid='YES' autocomplete='$line'>"
+		echo "    <title>$line</title>"
+		echo "    <subtitle>Open '$line' in Filezilla</subtitle>"
+		echo "     <icon type='fileicon'>/Applications/Filezilla.app</icon> "
+		echo "</item>"
+	fi
+
+done
+
+echo "</items>"
